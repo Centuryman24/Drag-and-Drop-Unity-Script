8aab97a10771f9e17aa0f111288184105ac5b633
Centuryman24 <45721488+Centuryman24@users.noreply.github.com>
2021-03-27 11:00:25 -0500

    Add files via upload

---
+++ DragAndDropSC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropSC : MonoBehaviour
+{
+    //For the script to work, you must put it in an object that you can drag and that the main camera has the tag "MainCamara"
+    public Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2.5f);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+        rb.isKinematic = true;
+
+    }
+    private void OnMouseUp()
+    {
+        rb.isKinematic = false;
+    }
+}
